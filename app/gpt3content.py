d1836d765321188fd21c77fd92b51a608b2a0235
ikathuria <ishani@kathuria.net>
2022-06-25 19:48:37 +0530

    shhh

--- app/gpt3content.py
+++ app/gpt3content.py
@@ -1,10 +1,8 @@
 import os
 import openai
-from dotenv import load_dotenv
+from app import config
 
-load_dotenv()
-openai.api_key = os.getenv('OPENAI_API_KEY')
-completion = openai.Completion()
+openai.api_key = config.OPENAI_API_KEY
 
 
 def generate_blog(prompt):
